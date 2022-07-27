0bd15f3d86b8c0ff3e061569cca5988b1499edc7
Marcus Talbott <mtalbott@broadinstitute.org>
2022-07-27 18:59:34 -0400

    add sentry configuration and emit 5xx errors to sentry

--- service/src/main/java/bio/terra/profile/app/StartupInitializer.java
+++ service/src/main/java/bio/terra/profile/app/StartupInitializer.java
@@ -3,11 +3,16 @@ package bio.terra.profile.app;
 import bio.terra.common.migrate.LiquibaseMigrator;
 import bio.terra.profile.app.configuration.AzureConfiguration;
 import bio.terra.profile.app.configuration.ProfileDatabaseConfiguration;
+import bio.terra.profile.app.configuration.SentryConfiguration;
 import bio.terra.profile.service.job.JobService;
+import io.sentry.Sentry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 public final class StartupInitializer {
   private static final String changelogPath = "db/changelog.xml";
+  private static final Logger logger = LoggerFactory.getLogger(StartupInitializer.class);
 
   public static void initialize(ApplicationContext applicationContext) {
     // Initialize or upgrade the database depending on the configuration
@@ -15,6 +20,7 @@ public final class StartupInitializer {
     JobService jobService = applicationContext.getBean(JobService.class);
     ProfileDatabaseConfiguration profileDatabaseConfiguration =
         applicationContext.getBean(ProfileDatabaseConfiguration.class);
+    SentryConfiguration sentryConfiguration = applicationContext.getBean(SentryConfiguration.class);
     AzureConfiguration azureConfiguration = applicationContext.getBean(AzureConfiguration.class);
     azureConfiguration.logOffers();
 
@@ -28,6 +34,14 @@ public final class StartupInitializer {
     // The JobService initialization also handles Stairway initialization.
     jobService.initialize();
 
+    if (sentryConfiguration.dsn().isEmpty()) {
+      logger.info("No Sentry DSN found. Starting up without it.");
+    } else {
+      Sentry.init(
+              options -> {
+                options.setDsn(sentryConfiguration.dsn());
+              });
+    }
     // TODO: Fill in this method with any other initialization that needs to happen
     //  between the point of having the entire application initialized and
     //  the point of opening the port to start accepting REST requests.
--- service/src/main/java/bio/terra/profile/app/controller/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/profile/app/controller/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package bio.terra.profile.app.controller;
 
 import bio.terra.common.exception.AbstractGlobalExceptionHandler;
 import bio.terra.profile.model.ErrorReport;
-import java.util.List;
+import io.sentry.Sentry;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 /**
  * This module provides a top-level exception handler for controllers. All exceptions that rise
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<ErrorReport> {
   @Override
   public ErrorReport generateErrorReport(Throwable ex, HttpStatus statusCode, List<String> causes) {
+    if (statusCode.is5xxServerError()) {
+      Sentry.captureException(ex);
+    }
     return new ErrorReport().message(ex.getMessage()).statusCode(statusCode.value()).causes(causes);
   }
 }
