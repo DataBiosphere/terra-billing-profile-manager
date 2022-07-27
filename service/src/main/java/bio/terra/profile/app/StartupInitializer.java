b3c4447b5d7274eb155bc86b27ab14f7039f1b34
Marcus Talbott <mtalbott@broadinstitute.org>
2022-07-27 18:59:34 -0400

    add sentry configuration and emit 5xx errors to sentry

--- service/src/main/java/bio/terra/profile/app/StartupInitializer.java
+++ service/src/main/java/bio/terra/profile/app/StartupInitializer.java
@@ -2,11 +2,16 @@ package bio.terra.profile.app;
 
 import bio.terra.common.migrate.LiquibaseMigrator;
 import bio.terra.profile.app.configuration.ProfileDatabaseConfiguration;
+import bio.terra.profile.app.configuration.SentryConfiguration;
 import bio.terra.profile.service.job.JobService;
+import io.sentry.Sentry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 public final class StartupInitializer {
   private static final String changelogPath = "db/changelog.xml";
+  private static final Logger logger = LoggerFactory.getLogger(StartupInitializer.class);
 
   public static void initialize(ApplicationContext applicationContext) {
     // Initialize or upgrade the database depending on the configuration
@@ -14,6 +19,7 @@ public final class StartupInitializer {
     JobService jobService = applicationContext.getBean(JobService.class);
     ProfileDatabaseConfiguration profileDatabaseConfiguration =
         applicationContext.getBean(ProfileDatabaseConfiguration.class);
+    SentryConfiguration sentryConfiguration = applicationContext.getBean(SentryConfiguration.class);
 
     // Migrate the database
     if (profileDatabaseConfiguration.isInitializeOnStart()) {
@@ -25,6 +31,14 @@ public final class StartupInitializer {
     // The JobService initialization also handles Stairway initialization.
     jobService.initialize();
 
+    if (sentryConfiguration.dsn().isEmpty()) {
+      logger.info("No Sentry DSN found. Starting up without it.");
+    } else {
+      Sentry.init(
+              options -> {
+                options.setDsn(sentryConfiguration.dsn());
+              });
+    }
     // TODO: Fill in this method with any other initialization that needs to happen
     //  between the point of having the entire application initialized and
     //  the point of opening the port to start accepting REST requests.
