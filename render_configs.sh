cc07e9c021b7df24a0b55a20e8e525ab995c8b3f
Blake Geno <bgeno@broadinstitute.org>
2022-08-17 10:54:16 -0400

    switch to using dsde-toolbox to access vault

--- render_configs.sh
+++ render_configs.sh
@@ -5,11 +5,9 @@ VAULT_TOKEN=${2:-$(cat "$HOME"/.vault-token)}
 
 VAULT_ADDR="https://clotho.broadinstitute.org:8200"
 
-VAULT_DOCKER_IMAGE="vault:1.11.2"
-
 LOCAL_CONFIG_OUTPUT="service/src/main/resources/generated/local-properties.yaml"
 
-VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR $VAULT_DOCKER_IMAGE vault read"
+VAULT_COMMAND="docker run --rm -i -e VAULT_TOKEN="${VAULT_TOKEN}" broadinstitute/dsde-toolbox:dev vault read"
 
 function azure_creds {
   AZURE_VAULT_PATH="secret/dsde/terra/azure/dev/billingprofilemanager/managed-app-publisher"
