95de53989d3130181fddd980e2625d5e03cdae91
Marc Talbott <mtalbott@broadinstitute.org>
2022-09-01 15:29:19 -0400

    [WOR-497] set environment in sentry configuration (#32)

--- service/src/main/java/bio/terra/profile/app/configuration/SentryConfiguration.java
+++ service/src/main/java/bio/terra/profile/app/configuration/SentryConfiguration.java
@@ -3,4 +3,4 @@ package bio.terra.profile.app.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "profile.sentry")
-public record SentryConfiguration(String dsn) {}
+public record SentryConfiguration(String dsn, String environment) {}
