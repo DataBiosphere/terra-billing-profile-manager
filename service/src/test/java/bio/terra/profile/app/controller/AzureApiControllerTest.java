56eee359a950146073466528a146080f8a2e42de
Drew Herbst <aherbst@broadinstitute.org>
2022-10-04 09:31:14 -0400

    Add azure controller test

--- service/src/test/java/bio/terra/profile/app/controller/AzureApiControllerTest.java
+++ service/src/test/java/bio/terra/profile/app/controller/AzureApiControllerTest.java
@@ -22,54 +22,54 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class AzureApiControllerTest extends BaseSpringUnitTest {
 
-    @Autowired MockMvc mockMvc;
+  @Autowired MockMvc mockMvc;
 
-    @MockBean SamService samService;
-    @MockBean AzureService azureService;
-    AuthenticatedUserRequest userRequest =
-            AuthenticatedUserRequest.builder()
-                    .setEmail("example@example.com")
-                    .setSubjectId("fake_sub")
-                    .setToken("fake_token")
-                    .build();
+  @MockBean SamService samService;
+  @MockBean AzureService azureService;
+  private final AuthenticatedUserRequest userRequest =
+      AuthenticatedUserRequest.builder()
+          .setEmail("example@example.com")
+          .setSubjectId("fake_sub")
+          .setToken("fake_token")
+          .build();
 
-    @BeforeEach
-    void setup() throws Exception {
-        when(samService.getUserStatusInfo(eq(userRequest.getToken())))
-                .thenReturn(
-                        new UserStatusInfo()
-                                .userSubjectId(userRequest.getSubjectId())
-                                .userEmail(userRequest.getEmail())
-                                .enabled(true));
-    }
+  @BeforeEach
+  void setup() throws Exception {
+    when(samService.getUserStatusInfo(eq(userRequest.getToken())))
+        .thenReturn(
+            new UserStatusInfo()
+                .userSubjectId(userRequest.getSubjectId())
+                .userEmail(userRequest.getEmail())
+                .enabled(true));
+  }
 
-    @Test
-    void getManagedApps_returnsOk() throws Exception {
-        var subId = UUID.randomUUID();
-        mockMvc
-                .perform(
-                        get("/api/azure/v1/managedApps")
-                                .queryParam("azureSubscriptionId", subId.toString())
-                                .header("Authorization", "Bearer " + userRequest.getToken()))
-                .andExpect(status().is(HttpStatus.OK.value()));
-    }
+  @Test
+  void getManagedApps_returnsOk() throws Exception {
+    var subId = UUID.randomUUID();
+    mockMvc
+        .perform(
+            get("/api/azure/v1/managedApps")
+                .queryParam("azureSubscriptionId", subId.toString())
+                .header("Authorization", "Bearer " + userRequest.getToken()))
+        .andExpect(status().is(HttpStatus.OK.value()));
+  }
 
-    @Test
-    void getManagedApps_returnsBadRequestWithNoUUID() throws Exception {
-        mockMvc
-                .perform(
-                        get("/api/azure/v1/managedApps")
-                                .header("Authorization", "Bearer " + userRequest.getToken()))
-                .andExpect(status().is(HttpStatus.BAD_REQUEST.value()));
-    }
+  @Test
+  void getManagedApps_returnsBadRequestWithNoUUID() throws Exception {
+    mockMvc
+        .perform(
+            get("/api/azure/v1/managedApps")
+                .header("Authorization", "Bearer " + userRequest.getToken()))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()));
+  }
 
-    @Test
-    void getManagedApps_returnsBadRequestWithBadUUID() throws Exception {
-        mockMvc
-                .perform(
-                        get("/api/azure/v1/managedApps")
-                                .queryParam("azureSubscriptionId", "baduuid")
-                                .header("Authorization", "Bearer " + userRequest.getToken()))
-                .andExpect(status().is(HttpStatus.BAD_REQUEST.value()));
-    }
+  @Test
+  void getManagedApps_returnsBadRequestWithBadUUID() throws Exception {
+    mockMvc
+        .perform(
+            get("/api/azure/v1/managedApps")
+                .queryParam("azureSubscriptionId", "baduuid")
+                .header("Authorization", "Bearer " + userRequest.getToken()))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()));
+  }
 }
