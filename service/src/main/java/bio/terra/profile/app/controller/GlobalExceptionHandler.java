17521a03855b2205c0ec85a1a46a30bc9f58437f
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2022-10-05 14:00:56 -0400

    [WOR-551] Add azure controller test (#49)

--- service/src/main/java/bio/terra/profile/app/controller/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/profile/app/controller/GlobalExceptionHandler.java
@@ -5,7 +5,11 @@ import bio.terra.profile.model.ErrorReport;
 import io.sentry.Sentry;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * This module provides a top-level exception handler for controllers. All exceptions that rise
@@ -20,5 +24,18 @@ public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<Error
       Sentry.captureException(ex);
     }
     return new ErrorReport().message(ex.getMessage()).statusCode(statusCode.value()).causes(causes);
+  }
+
+  @ExceptionHandler({
+    MethodArgumentTypeMismatchException.class,
+    MissingServletRequestParameterException.class
+  })
+  public ResponseEntity<ErrorReport> validationExceptionHandler(Exception ex) {
+    var errorReport =
+        new ErrorReport()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .message("Invalid request " + ex.getClass().getSimpleName());
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorReport);
   }
 }
--- service/src/test/java/bio/terra/profile/common/BaseUnitTest.java
+++ service/src/test/java/bio/terra/profile/common/BaseUnitTest.java
@@ -4,5 +4,5 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
 @Tag("unit")
-@ActiveProfiles({"test", "unit"})
+@ActiveProfiles({"test", "unit", "human-readable-logging"})
 public class BaseUnitTest {}
