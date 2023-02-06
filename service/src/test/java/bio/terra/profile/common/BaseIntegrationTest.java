ca57192cd5a709d3ff0c52cbea9b4bf60c9fa9c2
aherbst-broad <aherbst@broadinstitute.org>
2023-02-06 14:39:45 -0500

    Adjust workflows

--- service/src/test/java/bio/terra/profile/common/BaseIntegrationTest.java
+++ service/src/test/java/bio/terra/profile/common/BaseIntegrationTest.java
@@ -1,6 +1,14 @@
 package bio.terra.profile.common;
 
+import bio.terra.profile.app.Main;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Tag("integration")
-public class BaseIntegrationTest extends BaseSpringUnitTest {}
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Main.class)
+@SpringBootTest
+public class BaseIntegrationTest {}
