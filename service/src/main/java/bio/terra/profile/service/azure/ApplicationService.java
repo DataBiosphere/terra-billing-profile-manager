fcfc1aef5774b14620f20cc43f53c6f797e00dd9
aherbst-broad <aherbst@broadinstitute.org>
2023-02-16 10:40:52 -0500

    [WOR-767] Handle management exceptions for inaccessible Azure subscriptions

--- service/src/main/java/bio/terra/profile/service/azure/ApplicationService.java
+++ service/src/main/java/bio/terra/profile/service/azure/ApplicationService.java
@@ -1,16 +1,24 @@
 package bio.terra.profile.service.azure;
 
+import bio.terra.profile.service.azure.exception.InaccessibleSubscriptionException;
 import bio.terra.profile.service.crl.CrlService;
 import bio.terra.profile.service.profile.exception.DuplicateTagException;
+import com.azure.core.management.exception.ManagementException;
 import com.azure.resourcemanager.managedapplications.models.Application;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationService {
+  public static final String AZURE_SUB_NOT_FOUND = "SubscriptionNotFound";
+  public static final String AZURE_AUTH_FAILED = "AuthorizationFailed";
+  public static final String INVALID_AUTH_TOKEN = "InvalidAuthenticationTokenTenant";
+
+  private static final Set<String> INACCCESSIBLE_SUB_CODES =
+      Set.of(AZURE_SUB_NOT_FOUND, AZURE_AUTH_FAILED, INVALID_AUTH_TOKEN);
+
   private final CrlService crlService;
 
   @Autowired
@@ -34,9 +42,17 @@ public class ApplicationService {
    *
    * @param subscriptionId Azure subscription ID to be queried
    * @return UUID for the associated tenant
+   * @throws InaccessibleSubscriptionException when the azure subscription is not accessible by BPM
    */
   public UUID getTenantForSubscription(UUID subscriptionId) {
-    return crlService.getTenantForSubscription(subscriptionId);
+    try {
+      return crlService.getTenantForSubscription(subscriptionId);
+    } catch (ManagementException e) {
+      if (INACCCESSIBLE_SUB_CODES.contains(e.getValue().getCode())) {
+        throw new InaccessibleSubscriptionException("Subscription not accessible", e);
+      }
+      throw e;
+    }
   }
 
   /**
--- service/src/test/java/bio/terra/profile/service/azure/ApplicationServiceUnitTest.java
+++ service/src/test/java/bio/terra/profile/service/azure/ApplicationServiceUnitTest.java
@@ -4,14 +4,57 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import bio.terra.profile.common.BaseUnitTest;
+import bio.terra.profile.service.azure.exception.InaccessibleSubscriptionException;
 import bio.terra.profile.service.crl.CrlService;
 import bio.terra.profile.service.profile.exception.DuplicateTagException;
+import com.azure.core.management.exception.ManagementError;
+import com.azure.core.management.exception.ManagementException;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
 import java.util.Map;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 class ApplicationServiceUnitTest extends BaseUnitTest {
+  @Test
+  void getTenantForSubscription_accessibleSubscription() {
+    var crlService = mock(CrlService.class);
+    var expectedTenantId = UUID.randomUUID();
+    when(crlService.getTenantForSubscription(any(UUID.class))).thenReturn(expectedTenantId);
+    var applicationService = new ApplicationService(crlService);
+
+    var result = applicationService.getTenantForSubscription(UUID.randomUUID());
+
+    assert (result.equals(expectedTenantId));
+  }
+
+  @Test
+  void getTenantForSubscription_inaccessibleSubscription() {
+    var crlService = mock(CrlService.class);
+    when(crlService.getTenantForSubscription(any(UUID.class)))
+        .thenThrow(
+            new ManagementException(
+                "error",
+                null,
+                new ManagementError(ApplicationService.AZURE_SUB_NOT_FOUND, "not found")));
+    var applicationService = new ApplicationService(crlService);
+
+    assertThrows(
+        InaccessibleSubscriptionException.class,
+        () -> applicationService.getTenantForSubscription(UUID.randomUUID()));
+  }
+
+  @Test
+  void getTenantForSubscription_otherMgmtError() {
+    var crlService = mock(CrlService.class);
+    when(crlService.getTenantForSubscription(any(UUID.class)))
+        .thenThrow(
+            new ManagementException("error", null, new ManagementError("ExampleError", "example")));
+    var applicationService = new ApplicationService(crlService);
+
+    assertThrows(
+        ManagementException.class,
+        () -> applicationService.getTenantForSubscription(UUID.randomUUID()));
+  }
 
   @Test
   void addTagToMrg() {
