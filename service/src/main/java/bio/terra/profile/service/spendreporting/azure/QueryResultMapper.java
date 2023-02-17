67fbf37c70b2b7a5de4f3de8fb72e64d46225d5f
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-02-17 14:22:26 -0800

    Add validation for cost management data mapping. Add category mapper.

--- service/src/main/java/bio/terra/profile/service/spendreporting/azure/QueryResultMapper.java
+++ service/src/main/java/bio/terra/profile/service/spendreporting/azure/QueryResultMapper.java
@@ -1,43 +1,69 @@
 package bio.terra.profile.service.spendreporting.azure;
 
-import bio.terra.profile.service.spendreporting.azure.model.SpendCategoryType;
+import bio.terra.profile.service.spendreporting.azure.exception.UnexpectedCostManagementQueryResponse;
 import bio.terra.profile.service.spendreporting.azure.model.SpendData;
 import bio.terra.profile.service.spendreporting.azure.model.SpendDataItem;
+import com.azure.resourcemanager.costmanagement.models.QueryColumn;
 import com.azure.resourcemanager.costmanagement.models.QueryResult;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QueryResultMapper {
-  private static final int ROW_COST_COLUMN_INDEX = 0;
-  private static final int ROW_RESOURCE_TYPE_COLUMN_INDEX = 1;
-  private static final int ROW_CURRENCY_COLUMN_INDEX = 2;
+  private static final int COST_COLUMN_INDEX = 0;
+  private static final int RESOURCE_TYPE_COLUMN_INDEX = 1;
+  private static final int CURRENCY_COLUMN_INDEX = 2;
 
-  private static final String AZURE_COMPUTE_RESOURCE_TYPE = "microsoft.compute";
-  private static final String AZURE_STORAGE_RESOURCE_TYPE = "microsoft.storage";
+  private static final String COST_COLUMN_NAME = "cost";
+  private static final String RESOURCE_TYPE_COLUMN_NAME = "resourcetype";
+  private static final String CURRENCY_COLUMN_NAME = "currency";
+
+  private static final int EXPECTED_NUMBER_OF_COLUMNS = 3;
+
+  private SpendDataItemCategoryMapper spendDataItemCategoryMapper;
+
+  public QueryResultMapper(SpendDataItemCategoryMapper spendDataItemCategoryMapper) {
+    this.spendDataItemCategoryMapper = spendDataItemCategoryMapper;
+  }
 
   public SpendData mapQueryResult(QueryResult queryResult) {
+    throwIfDataFormatIsNotValid(queryResult);
+
     var spendItems =
         queryResult.rows().stream()
             .map(
                 r ->
                     new SpendDataItem(
-                        r.get(ROW_RESOURCE_TYPE_COLUMN_INDEX).toString(),
-                        Double.parseDouble(r.get(ROW_COST_COLUMN_INDEX).toString()),
-                        r.get(ROW_CURRENCY_COLUMN_INDEX).toString(),
-                        mapResourceCategory(r.get(ROW_RESOURCE_TYPE_COLUMN_INDEX).toString())))
+                        r.get(RESOURCE_TYPE_COLUMN_INDEX).toString(),
+                        Double.parseDouble(r.get(COST_COLUMN_INDEX).toString()),
+                        r.get(CURRENCY_COLUMN_INDEX).toString(),
+                        spendDataItemCategoryMapper.mapResourceCategory(
+                            r.get(RESOURCE_TYPE_COLUMN_INDEX).toString())))
             .collect(Collectors.toList());
 
     return new SpendData(spendItems);
   }
 
-  private SpendCategoryType mapResourceCategory(String resourceCategory) {
-    if (resourceCategory.startsWith(AZURE_COMPUTE_RESOURCE_TYPE)) {
-      return SpendCategoryType.COMPUTE;
-    } else if (resourceCategory.startsWith(AZURE_STORAGE_RESOURCE_TYPE)) {
-      return SpendCategoryType.STORAGE;
-    } else {
-      return SpendCategoryType.OTHER;
+  private void throwIfDataFormatIsNotValid(QueryResult queryResult) {
+    if (queryResult.columns().size() != EXPECTED_NUMBER_OF_COLUMNS) {
+      throw new UnexpectedCostManagementQueryResponse(
+          String.format(
+              "Unexpected number of columns: actual=%s, expected=%s",
+              queryResult.columns().size(), EXPECTED_NUMBER_OF_COLUMNS));
+    }
+    if (!(StringUtils.equalsIgnoreCase(
+            queryResult.columns().get(COST_COLUMN_INDEX).name(), COST_COLUMN_NAME)
+        || StringUtils.equalsIgnoreCase(
+            queryResult.columns().get(COST_COLUMN_INDEX).name(), RESOURCE_TYPE_COLUMN_NAME)
+        || StringUtils.equalsIgnoreCase(
+            queryResult.columns().get(COST_COLUMN_INDEX).name(), CURRENCY_COLUMN_NAME))) {
+      throw new UnexpectedCostManagementQueryResponse(
+          String.format(
+              "One of the expected columns not found. Columns=[%s]",
+              queryResult.columns().stream()
+                  .map(QueryColumn::name)
+                  .collect(Collectors.joining(","))));
     }
   }
 }
---
+++ service/src/main/java/bio/terra/profile/service/spendreporting/azure/SpendDataItemCategoryMapper.java
@@ -0,0 +1,20 @@
+package bio.terra.profile.service.spendreporting.azure;
+
+import bio.terra.profile.service.spendreporting.azure.model.SpendCategoryType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpendDataItemCategoryMapper {
+  private static final String AZURE_COMPUTE_RESOURCE_TYPE = "microsoft.compute";
+  private static final String AZURE_STORAGE_RESOURCE_TYPE = "microsoft.storage";
+
+  public SpendCategoryType mapResourceCategory(String resourceCategory) {
+    if (resourceCategory.startsWith(AZURE_COMPUTE_RESOURCE_TYPE)) {
+      return SpendCategoryType.COMPUTE;
+    } else if (resourceCategory.startsWith(AZURE_STORAGE_RESOURCE_TYPE)) {
+      return SpendCategoryType.STORAGE;
+    } else {
+      return SpendCategoryType.OTHER;
+    }
+  }
+}
