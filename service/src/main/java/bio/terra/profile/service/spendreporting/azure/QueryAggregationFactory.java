f1e154740d69a0368cf20b11756eeafe80793c2a
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-02-24 10:36:53 -0800

    Address sonar bug

--- service/src/main/java/bio/terra/profile/service/spendreporting/azure/QueryAggregationFactory.java
+++ service/src/main/java/bio/terra/profile/service/spendreporting/azure/QueryAggregationFactory.java
@@ -7,16 +7,9 @@ import java.util.Map;
 
 public class QueryAggregationFactory {
   public static Map<String, QueryAggregation> buildDefault() {
-    return new HashMap<>() {
-      {
-        put("totalCost", new QueryAggregation().withName("Cost").withFunction(FunctionType.SUM));
-      }
-
-      //      {
-      //        put(
-      //            "totalCostUSD",
-      //            new QueryAggregation().withName("CostUSD").withFunction(FunctionType.SUM));
-      //      }
-    };
+    Map<String, QueryAggregation> queryAggregation = new HashMap<>();
+    queryAggregation.put(
+        "totalCost", new QueryAggregation().withName("Cost").withFunction(FunctionType.SUM));
+    return queryAggregation;
   }
 }
