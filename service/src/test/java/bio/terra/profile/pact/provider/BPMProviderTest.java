d60ca49a1501d8bcc573fa8d244312085e49b03a
Blake Geno <bgeno@broadinstitute.org>
2023-06-02 14:48:36 -0400

    spotless

--- service/src/test/java/bio/terra/profile/pact/provider/BPMProviderTest.java
+++ service/src/test/java/bio/terra/profile/pact/provider/BPMProviderTest.java
@@ -7,9 +7,6 @@ import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
@@ -40,11 +37,10 @@ import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @Tag("provider-test")
 @Provider("bpm-provider")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"bio.terra.profile.app.controller", "bio.terra.profile.service"})
 @ConfigurationPropertiesScan(basePackages = {"bio.terra.profile"})
 @ExtendWith(SpringExtension.class)
