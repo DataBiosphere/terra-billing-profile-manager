28df7e337e08c4604d5160d443b266a6b9b287af
Blake Geno <bgeno@broadinstitute.org>
2023-06-05 10:27:01 -0400

    removed extra annotation

--- service/src/test/java/bio/terra/profile/common/BaseSpringUnitTest.java
+++ service/src/test/java/bio/terra/profile/common/BaseSpringUnitTest.java
@@ -1,13 +1,10 @@
 package bio.terra.profile.common;
 
 import bio.terra.profile.app.Main;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /** Base class for unit tests requiring Spring functionality (autowiring, etc.) */
-@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Main.class)
 @SpringBootTest
 public class BaseSpringUnitTest extends BaseUnitTest {}
