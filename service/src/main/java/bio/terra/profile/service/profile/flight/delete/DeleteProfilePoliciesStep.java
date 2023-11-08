261774cf02626f179f883509d670f9dd15574236
Marcus Talbott <mtalbott@broadinstitute.org>
2023-11-08 15:58:15 -0500

    don't undelete profile policies

--- service/src/main/java/bio/terra/profile/service/profile/flight/delete/DeleteProfilePoliciesStep.java
+++ service/src/main/java/bio/terra/profile/service/profile/flight/delete/DeleteProfilePoliciesStep.java
@@ -5,9 +5,13 @@ import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public record DeleteProfilePoliciesStep(TpsApiDispatch tpsApiDispatch, UUID profileId)
     implements Step {
+
+  private static final Logger logger = LoggerFactory.getLogger(DeleteProfilePoliciesStep.class);
 
   @Override
   public StepResult doStep(FlightContext context) throws InterruptedException {
@@ -17,7 +21,8 @@ public record DeleteProfilePoliciesStep(TpsApiDispatch tpsApiDispatch, UUID prof
 
   @Override
   public StepResult undoStep(FlightContext context) throws InterruptedException {
-    // todo: re-create policies?
-    return StepResult.getStepResultSuccess();
+    // We can't un-delete the PAO, so just surface the error that caused the flight to fail.
+    logger.error("Unable to undo deletion of policies on profile {} ", profileId);
+    return context.getResult();
   }
 }
