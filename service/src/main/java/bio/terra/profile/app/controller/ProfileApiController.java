4d20d8ecd0c1de17ab87865dcf87e58a1f950d6a
aherbst-broad <aherbst@broadinstitute.org>
2023-12-04 10:57:52 -0500

    Testing

--- service/src/main/java/bio/terra/profile/app/controller/ProfileApiController.java
+++ service/src/main/java/bio/terra/profile/app/controller/ProfileApiController.java
@@ -32,6 +32,7 @@ public class ProfileApiController implements ProfileApi {
     this.request = request;
     this.profileService = profileService;
     this.authenticatedUserRequestFactory = authenticatedUserRequestFactory;
+    // IGNORE
   }
 
   @Override
