b2ad3fd8812ff559d879830e4c9912a532d88f20
Marcus Talbott <mtalbott@broadinstitute.org>
2024-03-20 12:07:26 -0400

    get google access token out of b2c jwt before making Google billing calls

--- service/src/main/java/bio/terra/profile/service/crl/GcpCrlService.java
+++ service/src/main/java/bio/terra/profile/service/crl/GcpCrlService.java
@@ -6,7 +6,9 @@ import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.profile.service.crl.exception.CrlInternalException;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.nimbusds.jwt.SignedJWT;
 import java.io.IOException;
+import java.text.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 public class GcpCrlService {
 
   private final ClientConfig clientConfig;
+  private static final String GOOGLE_ACCESS_TOKEN_CLAIM = "idp_access_token";
 
   @Autowired
   public GcpCrlService(ClientConfig clientConfig) {
@@ -24,7 +27,8 @@ public class GcpCrlService {
   /** Returns a GCP {@link CloudBillingClientCow} which wraps Google Billing API. */
   public CloudBillingClientCow getBillingClientCow(AuthenticatedUserRequest user) {
     try {
-      return new CloudBillingClientCow(clientConfig, getUserCredentials(user.getToken()));
+      return new CloudBillingClientCow(
+          clientConfig, getUserCredentials(getGoogleAccessToken(user.getToken())));
     } catch (IOException e) {
       throw new CrlInternalException("Error creating billing client wrapper", e);
     }
@@ -32,5 +36,15 @@ public class GcpCrlService {
 
   private GoogleCredentials getUserCredentials(String token) {
     return GoogleCredentials.newBuilder().setAccessToken(new AccessToken(token, null)).build();
+  }
+
+  private String getGoogleAccessToken(String userToken) {
+    try {
+      var jwt = SignedJWT.parse(userToken);
+      return (String) jwt.getJWTClaimsSet().getClaim(GOOGLE_ACCESS_TOKEN_CLAIM);
+    } catch (ParseException e) {
+      // even if the token is not a valid JWT, it may still be a valid Google access token
+      return userToken;
+    }
   }
 }
