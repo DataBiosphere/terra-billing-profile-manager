cdeeac7b637300202874cd3588bf27af176774a9
aherbst-broad <aherbst@broadinstitute.org>
2024-06-07 11:14:55 -0400

    Testing

--- service/src/test/java/bio/terra/profile/common/MetricsUtilsUnitTest.java
+++ service/src/test/java/bio/terra/profile/common/MetricsUtilsUnitTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+// IGNORE
 class MetricsUtilsUnitTest extends BaseUnitTest {
 
   private SimpleMeterRegistry meterRegistry;
