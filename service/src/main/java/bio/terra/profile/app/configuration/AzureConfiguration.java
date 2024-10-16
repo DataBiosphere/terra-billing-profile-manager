cc7b3701e0b958138a2f071c271d9de62741de50
bennettn4 <nibennett@microsoft.com>
2024-10-16 16:22:22 -0400

    Add Support for AzureGov hosting

--- service/src/main/java/bio/terra/profile/app/configuration/AzureConfiguration.java
+++ service/src/main/java/bio/terra/profile/app/configuration/AzureConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "profile.azure")
 public record AzureConfiguration(
+    String azureEnvironment,
     String managedAppClientId,
     String managedAppClientSecret,
     String managedAppTenantId,
@@ -64,13 +65,19 @@ public record AzureConfiguration(
     }
   }
 
+
+
+
   public TokenCredential buildManagedAppCredentials() {
 
     ManagedIdentityCredential managedIdentityCredential =
-        new ManagedIdentityCredentialBuilder().clientId(managedAppWorkloadClientId).build();
+        new ManagedIdentityCredentialBuilder()
+                .clientId(managedAppWorkloadClientId)
+                .build();
 
     ClientSecretCredential servicePrincipalCredential =
         new ClientSecretCredentialBuilder()
+            .authorityHost(getAzureEnvironment().getActiveDirectoryEndpoint())
             .clientId(managedAppClientId)
             .clientSecret(managedAppClientSecret)
             .tenantId(managedAppTenantId)
@@ -109,4 +116,17 @@ public record AzureConfiguration(
   public Boolean getControlPlaneEnabled() {
     return controlPlaneEnabled;
   }
+
+  public AzureEnvironment getAzureEnvironment() {
+    switch(azureEnvironment)
+    {
+      case "AZURE" :
+        return AzureEnvironment.AZURE;
+      case "AZURE_GOV" :
+        return AzureEnvironment.AZURE_US_GOVERNMENT;
+      default :
+        throw new IllegalArgumentException(String.format("Unknown Azure environment: %s", azureEnvironment));
+    }
+  }
+
 }
