22886923ae5199f17abdb1dbfc7f5e5b6de054e6
bennettn4 <nibennett@microsoft.com>
2024-10-17 11:29:06 -0400

    debug azureEnvironment

--- service/src/main/java/bio/terra/profile/app/configuration/PolicyServiceConfiguration.java
+++ service/src/main/java/bio/terra/profile/app/configuration/PolicyServiceConfiguration.java
@@ -9,6 +9,8 @@ import com.google.auth.oauth2.ServiceAccountCredentials;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -23,6 +25,9 @@ public class PolicyServiceConfiguration {
   private String basePath;
   private String clientCredentialFilePath;
   private final AzureConfiguration azureConfiguration;
+
+  private static final Logger logger = LoggerFactory.getLogger(PolicyServiceConfiguration.class);
+
 
   private static final List<String> POLICY_SERVICE_ACCOUNT_SCOPES =
       List.of("openid", "email", "profile");
@@ -50,6 +55,8 @@ public class PolicyServiceConfiguration {
 
   public String getAccessToken() throws IOException {
     if (azureConfiguration.controlPlaneEnabled()) {
+      logger.info("Control Plane Enabled. azureEnvironmentADEndpoint: {} ", azureConfiguration.getAzureEnvironment().getActiveDirectoryEndpoint());
+
       TokenCredential credential = new DefaultAzureCredentialBuilder()
               .authorityHost(azureConfiguration.getAzureEnvironment().getActiveDirectoryEndpoint())
               .build();
